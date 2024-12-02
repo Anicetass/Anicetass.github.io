eb3037b79056d56ba5c26ba21a39a5b799980477
Anicetass <144654014+Anicetass@users.noreply.github.com>
2024-12-02 08:40:10 +0200

    Update dark_light_theme.js

--- dark_light_theme.js
+++ dark_light_theme.js
@@ -1,7 +1,6 @@
 const toggleButton = document.getElementById('theme-toggle');
 const body = document.body;
 
-// Toggle theme on button click
 toggleButton.addEventListener('click', () => {
     body.classList.toggle('dark-theme');
     localStorage.setItem('theme', body.classList.contains('dark-theme') ? 'dark' : 'light');

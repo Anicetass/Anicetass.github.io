60e2af033e282d273b47583e777637ba43b93862
Anicetass <144654014+Anicetass@users.noreply.github.com>
2024-12-02 08:48:52 +0200

    Update dark_light_theme.js

--- dark_light_theme.js
+++ dark_light_theme.js
@@ -3,5 +3,4 @@ const body = document.body;
 
 toggleButton.addEventListener('click', () => {
     body.classList.toggle('dark-theme');
-    localStorage.setItem('theme', body.classList.contains('dark-theme') ? 'dark' : 'light');
 });
